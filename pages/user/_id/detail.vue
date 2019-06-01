1778c04a16e1c8591777b4d361403c26204983cd
Shohei_Japan <kai.entertainment0304@gmail.com>
2019-06-01 17:20:56 +0900

    deleted test

---
+++ pages/user/_id/detail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>ID 番号 {{ id }} のユーザー情報</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    id() {
+      return this.$route.params.id;
+    }
+  }
+};
+</script>
