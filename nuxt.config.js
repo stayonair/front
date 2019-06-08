dd6d1a7c249df978056c4951e500d9e0d04fa674
Shohei_Japan <kai.entertainment0304@gmail.com>
2019-06-09 02:48:02 +0900

    add post page

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,9 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    '~/plugins/vue-material'
+  ],
 
   /*
    ** Axios module configuration
