a7fee6389f99eb56d916af21e62ca9f1770d1650
Shohei_Japan <kai.entertainment0304@gmail.com>
2019-06-09 04:02:44 +0900

    add comment

--- pages/news_feed.vue
+++ pages/news_feed.vue
@@ -107,16 +107,15 @@ export default {
       const timeDiff = Math.floor((new Date() - date) / 3600000)
       const dateDiff = Math.floor(timeDiff / 24)
 
-      if (dateDiff === 0) { // 24 時間以内
+      if (dateDiff === 0) { // 24 時間以内の更新の場合、時間を表示する
         return `${timeDiff} hour${timeDiff > 1 ? 's' : ''} ago`
       }
-      if (dateDiff > 30) { // 1 月以上前
+      if (dateDiff > 30) { // 1 月以上前の更新の場合、日付を表示する
         return `${date.getFullYear()} / ${date.getMonth()+1} / ${date.getDate()}`
       }
-      return `${dateDiff} day${dateDiff > 1 ? 's' : ''} ago`
+      return `${dateDiff} day${dateDiff > 1 ? 's' : ''} ago` // 24 時間以上 1 月以内の更新の場合、何日前の更新かを表示する
     },
     goToPostPage(id) {
-      console.log(this.$route)
       this.$router.push({ path: `posts/${id}` })
     }
   }
