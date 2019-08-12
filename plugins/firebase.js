1b51c8ebb2b76adeaf3d47a10decb6565d5aefa2
Ryuji <ryujineer@gmail.com>
2019-08-12 13:49:54 +0900

    fix firebase plugins

---
+++ plugins/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/firebase-storage'
+
+const config = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+export default firebase
