ae827ca1765aa3f47db11d471512dae0d3e5130c
Naoko3in4 <okashimochi@gimail.com>
2019-09-04 01:20:30 +0900

    defined audio-duration and audio-current-time

---
+++ components/Atoms/Icons/IconPlay.vue
@@ -0,0 +1,46 @@
+<template>
+  <svg
+    id="_x31_0"
+    class="icon--play"
+    :class="getClasses"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 512 512"
+    xml:space="preserve"
+  >
+    <g>
+      <path
+        class="st0"
+        d="M466.767,230.219L75.001,4.033C55.157-7.426,30.349,6.899,30.349,29.817v452.37
+        c0,22.918,24.808,37.239,44.653,25.781l391.766-226.183C486.613,270.325,486.613,241.678,466.767,230.219z"
+        fill="#fafbfd"
+      />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    className: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    getClasses() {
+      return this.className
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .icon--play {
+    width: 2rem;
+  }
+  .play--red {
+    fill: red;
+  }
+</style>
--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -6,7 +6,7 @@
           :post="post"
           class="post__thumbnail"
         />
-        <figure class="post__audio">
+        <!-- <figure class="post__audio">
           <audio
             class="audio"
             controls
@@ -15,11 +15,71 @@
           >
             <code>audio</code> element
           </audio>
+        </figure> -->
+        <figure class="post__audio">
+          <audio
+            ref="audio"
+            class="audio"
+            preload="metadata"
+          >
+            <code>audio</code> element
+          </audio>
         </figure>
+
+        <input
+          v-model="audioProgress"
+          class="input_range"
+          type="range"
+          min="0"
+          max="100"
+          step="5"
+        >
+        {{ audioCurrentTime }} / {{ audioDuration }}
+
         <div class="post__article">
           <p>
             {{ post.article }}
           </p>
+          <!-- コンポーネント化予定の audio タグ -->
+          <div>
+            <div
+              class="audio__wrapper"
+              :class="className"
+            >
+              <div
+                class="icon-title__container"
+                @click="changeAudioFooterSize()"
+              >
+                <div class="traveler-icon__container">
+                  <img
+                    :src="post.author.icon_url"
+                    class="traveler-icon"
+                  >
+                </div>
+                <div class="post__title">
+                  {{ post.title }}
+                  <div class="post_thumbnail__author_name">
+                    @{{ post.author.name }}
+                  </div>
+                </div>
+              </div>
+
+              <div
+                v-if="!isPlaying"
+                class="audio_controller--play"
+                @click="playAudio()"
+              >
+                <icon-play class="icon_play" />
+              </div>
+
+              <div
+                v-else
+                @click="pauseAudio()"
+              >
+                <button>pause</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -29,20 +89,73 @@
 <script>
 import { mapState } from 'vuex'
 import PostThumbnail from '~/components/Molecules/PostThumbnail'
+import IconPlay from '~/components/Atoms/Icons/IconPlay'
 
 export default {
   name: 'Post',
   layout: 'user',
   components: {
-    PostThumbnail
+    PostThumbnail,
+    IconPlay
   },
+  data: () => ({
+    className: '',
+    isPlaying: false,
+    audioProgress: 0,
+    audioDuration: null, // audioトータル時間
+    audioCurrentTime: null, // audio 経過時間
+    audioData: 'http://www.voice-pro.jp/announce/mp3/001-sibutomo.mp3'
+  }),
   computed: {
     ...mapState({
-      post: store => store.post.post
+      post: store => store.post.post,
     }),
     getParams() {
       return this.$route.params.id
     }
+  },
+  async mounted() {
+    const audio = new Audio(this.audioData)
+    this.$refs.audio = audio
+    await audio.load()
+    audio.onloadedmetadata = () => {
+      this.audioDuration = audio.duration
+    }
+  },
+  methods: {
+    changeAudioFooterSize() {
+      if (this.className === 'audio--open') {
+        this.className = 'audio--close'
+      } else {
+        this.className = "audio--open"
+      }
+    },
+    getDuration() {
+      console.log(this.$refs)
+      return this.$refs.audio.duration
+    },
+    playAudio() {
+      // console.log('playAudio の時の audio' + this.$refs.audio);
+      console.log(this.$refs.audio);
+      console.log('readyState ' + this.$refs.audio.readyState);
+      console.log('playクリックタイム ' + this.$refs.audio.currentTime);
+      this.audioCurrentTime = this.$refs.audio.currentTime
+
+      this.$refs.audio.play()
+      this.isPlaying = !this.isPlaying
+    },
+    pauseAudio() {
+      console.log('readyState ' + this.$refs.audio.readyState);
+      console.log('pauseクリックタイム ' + this.$refs.audio.currentTime);
+      this.audioCurrentTime = this.$refs.audio.currentTime
+
+      this.$refs.audio.pause()
+      this.isPlaying = !this.isPlaying
+    },
+    seekingAudio() {
+      console.log("seeking began")
+      console.log(this.audioProgress)
+    },
   }
 }
 </script>
@@ -107,5 +220,94 @@ export default {
   padding: 0 5rem;
   font-size: 1.4rem;
   line-height: 3rem;
+}
+
+// コンポーネント化予定の audio タグ について
+.audio__wrapper {
+  height: 7rem;
+  background: $color-primary;
+  color: $color-white;
+  position: fixed;
+  bottom: 5rem;
+  left: 0;
+  padding: 1rem;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.icon-title__container {
+  display: flex;
+  align-items: center;
+}
+
+.traveler-icon {
+  background: $color-white;
+  margin-top: 1rem;
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+}
+
+.post__title {
+  margin-left: 1rem;
+}
+
+.post_thumbnail__author_name {
+  font-size: 1.2rem;
+  line-height: 1rem;
+}
+
+.icon_play {
+  width: 3rem;
+}
+
+.audio--open {
+  height:20rem;
+  transition-property: height;
+  transition-duration: 0.2s;
+  transition-timing-function:ease-in-out;
+  -webkit-transition-property: height; // Google Chrome、Safari向け
+  -webkit-transition-duration:0.2s;
+  -webkit-transition-timing-function:ease-in-out;
+}
+
+.audio--close {
+  height: 7rem;
+  transition-property: height;
+  transition-duration:0.2s;
+  transition-timing-function:ease-in-out;
+  -webkit-transition-property: height;
+  -webkit-transition-duration:0.2s;
+  -webkit-transition-timing-function:ease-in-out;
+}
+.input_range{
+  width: 20rem;
+  margin-left: 1rem;
+  -webkit-appearance: none;
+  appearance: none;
+  background-color: #eaeaea;
+  height: 2px;
+  width: 70Vw;
+  border-radius: 6px;
+
+  &:focus,
+  &:active {
+    outline: none;
+  }
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    cursor: pointer;
+    position: relative;
+    width: 22px;
+    height: 22px;
+    display: block;
+    background-color: #07be19;
+    border-radius: 50%;
+    -webkit-border-radius: 50%;
+  }
 }
 </style>
