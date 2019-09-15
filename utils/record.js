02b8b37afcbef3358f45ded863849d22970ee836
Ryuji Yamada <45064837+ryutabi@users.noreply.github.com>
2019-09-15 21:55:19 +0900

    modified API WebAudioRecorder for supports on iOS (#64)

--- utils/record.js
+++ utils/record.js
@@ -1,35 +1,50 @@
-// if (!navigator.mediaDevices) {
-//   alert("mediaDevices is unavailable")
-// }
+let mediaRecorder // WebAudioRecorder object
+let localStream // stream from getUserMedia()
+let mediaStreamSorce // MediaStreamAudioSourceNode
 
-let mediaRecorder = null
-let localstream = {}
-
-function recStart() {
+// 録音開始時
+const startRecording = () => {
   navigator.mediaDevices
     .getUserMedia({ audio: true })
-    .then(function(stream) {
-      localstream = stream
-      mediaRecorder = new MediaRecorder(stream)
-      mediaRecorder.start()
+    .then(stream => {
+      const AudioContext = window.AudioContext || window.webkitAudioContext
+      const audioContext = new AudioContext({ sampleRate: 22000 }) // sampleRare = 22000 kHz
+
+      localStream = stream
+      mediaStreamSorce = audioContext.createMediaStreamSource(stream)
+
+      mediaRecorder = new WebAudioRecorder(mediaStreamSorce, {
+        workerDir: '/record/',
+        encoding: 'mp3',
+        numChannels: 2 // 2 is the default, mp3 encoding supports only 2
+      })
+
+      mediaRecorder.setOptions({
+        timeLimit: 1200, // Maximum record time(seconds)
+        mp3: { bitRate: 192 }, // bitRate for mp3 encording
+        encodeAfterRecord: true
+      })
+
+      mediaRecorder.startRecording()
     })
-    .catch(function(e) {
-      console.log(e)
+    .catch(e => {
+      console.error(e)
     })
 }
 
-function stopRecording() {
+// 録音停止時
+const stopRecording = () => {
   return new Promise(resolve => {
-    mediaRecorder.stop()
-    mediaRecorder.ondataavailable = function(e) {
-      resolve(e.data)
+    localStream.getAudioTracks()[0].stop()
+    mediaRecorder.finishRecording()
+    mediaRecorder.onComplete = (_, blob) => {
+      resolve(blob)
     }
-    localstream.getTracks().forEach(track => track.stop())
   })
 }
 
 const record = {
-  recStart,
+  startRecording,
   stopRecording
 }
 
