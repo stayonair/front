d86a908bc5d8907bdca62dadce8da9bf1a6b972d
Ryuji <ryujineer@gmail.com>
2019-09-22 03:21:28 +0900

    fixed middleware authtenticated

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -2,16 +2,32 @@ import firebase from '~/plugins/firebase'
 
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
 
-export default async ({ store }) => {
-  // store にログイン情報があるときは何もしない
-  if (store.state.auth.user) {
-    return
-  }
-
+export default async ({ route, store, redirect }) => {
   await firebase.auth().onAuthStateChanged(user => {
     // ログインしていたら、store に追加する
     if (user) {
       store.dispatch('auth/setUser', user)
     }
   })
+
+
+  // store にログイン情報があるとき
+  // 一般閲覧可能なページのときは、何もしない
+  if (
+    store.state.auth.user ||
+    route.path === '/news_feed' ||
+    route.path === '/search' ||
+    route.path === '/login' ||
+    route.path === '/signup' ||
+    route.path === '/signup_name'
+  ) {
+    return
+  }
+
+  // store にログイン情報がないとき
+  // ログインページにリダイレクト
+  if (!store.state.auth.user) {
+    return redirect('/login')
+  }
+
 }
